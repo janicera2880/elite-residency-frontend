32ab9ca2ecc6d98e6aa56cdb4ed02efce3af4637
Janice <janiceralecha@gmail.com>
2023-01-15 16:46:11 -0600

    Add scaffoldings

---
+++ src/components/App.js
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from "react";
+import { Route, Routes } from 'react-router-dom';
+import Home from './Home';
+import Header from './Header';
+import NavBar from'./NavBar';
+
+function App(){
+
+  const [listings, setListings] = useState([])
+}
+return (
+    <div>
+        
+        <Header />
+        <NavBar />
+
+      </div>
+            
+)
+          
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+
+
+function Header() {
+
+   
+   
+    return (
+      
+      
+      <div className="header">
+        <h1>Elite Residency</h1>
+      
+      </div>
+    
+  );
+}
+  
+  export default Header;
