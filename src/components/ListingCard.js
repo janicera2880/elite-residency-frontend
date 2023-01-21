048ef98a2f081235e819d7a723b8d6feb68102ad
Janice <janiceralecha@gmail.com>
2023-01-20 19:18:34 -0600

    Fix key props to Listing card and call back function

--- src/components/ListingCard.js
+++ src/components/ListingCard.js
@@ -3,7 +3,7 @@ import React, {useState} from "react";
 
 
 function ListingCard ({listing, changePrice}){
-  const {id, image, status, listprice, built, storey, bedroom, bathroom, garage, pool, buildingsize, lotsize, architecture} = listing
+  const {id, image, status, listPrice, yearBuilt, storey, bedroom, bathroom, garage, buildingSize, lotSize, architectureStyle, pool} = listing
   const [available, setAvailable] = useState(true)
   const [newPrice, setPrice] = useState(listprice)
 
@@ -18,16 +18,16 @@ function ListingCard ({listing, changePrice}){
       <div className="grid-item">
             <img src={image} width="600" height="300" alt={image}/>
             <p>Status:{status}</p>
-            <p>List Price:{listprice}</p>
-            <p>Year Built:{built}</p>
+            <p>List Price:{listPrice}</p>
+            <p>Year Built:{yearBuilt}</p>
             <p>Storey:{storey}</p>
             <p>Bedroom:{bedroom}</p>
             <p>Bathroom:{bathroom}</p>
             <p>Garage:{garage}</p>
+            <p>Building Size:{buildingSize}</p>
+            <p>Lot Size{lotSize}</p>
+            <p>Architectural Style:{architectureStyle}</p>
             <p>Pool:{pool ? "Yes" : "No"}</p>
-            <p>Building Size:{buildingsize}</p>
-            <p>Lot Size{lotsize}</p>
-            <p>Architectural Style:{architecture}</p>
          
 
 
@@ -36,7 +36,7 @@ function ListingCard ({listing, changePrice}){
             <input value={newPrice} onChange={onChangePrice}/>
             <button onClick={()=>changePrice(newPrice, id)}>Update</button>
             {available ? (
-            <button onClick={handleChangeStatus} className="availability">Available</button>
+            <button onClick={handleChangeStatus} className="availability">Continue To Show</button>
           ) : (
             <button onClick={handleChangeStatus}>Sold</button>
           )}
